79157bcba016193da152361e3aeefc8a1bbe17b8
Ari <ari.kaps@gmail.com>
2019-07-07 20:35:40 -0700

    Project 4: mult
    Successfully runs, but the visualizer is strange
    We decide on the correct number to decrease, then decrease
    the opposite one and arrive at the correct value
    occurs for 2/4 and 6/7

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,54 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+    //initialize sum to 0
+    M[R2]=0
+
+    //check if R0 is 0
+    D=M[R0]
+    @END
+    D; JEQ
+
+    //check if R1 is 0
+    D=M[R1]
+    @END
+    D; JEQ
+
+    D=M[R0]
+    D=D-M[R1]
+    @LOOP_DEC_R1
+    D; JLT // R0<R1, decreasing R0 is fewer loops than R1
+    //else, decreasing R1 is fewer loops than R0. Fall through
+
+(LOOP_DEC_R0)
+    //add R1 to the running total
+    D=M[R1]
+    D=D+M[R2]
+    M[R2]=D
+    //decrement R0
+    D=M[R0]
+    D=D-1
+    @END
+    D; JLE // R0 is <= 0
+    M[R0]=D
+    @LOOP_DEC_R0
+    D; JMP
+
+(LOOP_DEC_R1)
+    //add R0 to the running total
+    D=M[R0]
+    D=D+M[R2]
+    M[R2]=D
+    //decrement R1
+    D=M[R1]
+    D=D-1
+    @END
+    D; JLE // R1 is <= 0
+    M[R1]=D
+    @LOOP_DEC_R1
+    D; JMP
+
+(END)
+    @END
+    0; JMP
