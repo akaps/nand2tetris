9156d291afb4e94de10d40510805e1bb849fa431
Ari <ari.kaps@gmail.com>
2019-07-18 02:18:13 -0700

    Beginning to implement Translator
    1. Translator can also take directories, support it
    1a. os is a bad library for file manipulations, use pathlib instead
    2. Begin structuring translate function
    3. Implementation for Parser
    4. File IO implementation for CodeWriter. No assembly
    5. I like single quotes. Replacing doubles
    
    Each of these should be its own commit. Have more discipline

--- projects/07/VMTranslator.py
+++ projects/07/VMTranslator.py
@@ -1,109 +1,186 @@
 import sys
-import os
+from pathlib import Path
 from enum import Enum
 
+SEGMENTS = {
+    'local' : 'LCL',
+    'argument' : 'ARG',
+    'this' : 'THIS',
+    'that' : 'THAT',
+    'pointer' : 'THIS',
+    'temp' : 'TEMP'
+}
+
+ARITHMETIC = [
+    'add',
+    'sub',
+    'neg',
+    'eq',
+    'gt',
+    'lt',
+    'and',
+    'or',
+    'not'
+]
+
+#additional VM commands
+PUSH = 'push'
+POP = 'pop'
+
+VM_EXTENSION = '.vm'
+ASSEMBLY_EXTENSION = '.asm'
+STACK_START = 0x100
+HEAP_START = 0x800
+
+#commonly used delimeters
+COMMENT = '//'
+
 class CommandType(Enum):
-    C_ARITHMETIC = 0,
-    C_PUSH = 1,
-    C_POP = 2,
-    C_LABEL = 3,
-    C_GOTO = 4,
-    C_IF = 5,
-    C_FUNCTION = 6,
-    C_RETURN = 7
-    C_CALL = 8
+    C_ARITHMETIC = 0 #form: add/sub/neg/eq/gt/lt/and/or/not
+    C_PUSH = 1 #form: push segment index. push value of segment[index] to stack
+    C_POP = 2 #form: pop segment index. pop topmost stack element and store in segment[index]
+    C_LABEL = 3 #label for goto
+    C_GOTO = 4 #unconditional branching
+    C_IF = 5 #conditional branching
+    C_FUNCTION = 6 #form: functionName nLocals. includes number of variables
+    C_RETURN = 7 #transfer control back to calling function
+    C_CALL = 8 #form: call functionName nArgs
 
 class Parser:
-    """
+    '''
     Handles the parsing of a single .vm file by reading each VM command and parsing it into its lexical components
-    """
-    def __init__(self, lines):
-        """
+    '''
+    def __init__(self, file_name):
+        '''
         Prepares to parse the input vm lines
-        """
-        pass
+        '''
+        file = open(file_name, 'r')
+        self.lines = self.remove_decorators(file.readlines())
+        file.close()
+        self.next_command = 0
+        self.current_command = None
+
+    def remove_decorators(self, lines):
+        result = []
+        for line in lines:
+            line=line.strip()
+            if COMMENT in line:
+                comment_index= line.index(COMMENT)
+                line = line[:comment_index]
+            if line:
+                result.append(line.strip())
+        return result
 
     def has_more_commands(self):
-        """
+        '''
         Returns true if there are more commands in the input
-        """
-        pass
+        '''
+        return self.next_command < len(self.lines)
 
     def advance(self):
-        """
+        '''
         Reads the next command from the input and makes it the current command
-        """
-        pass
+        '''
+        self.current_command = self.lines[self.next_command]
+        self.next_command += 1
 
     def command_type(self):
-        """
+        '''
         Returns a constant representing the type of the current command
-        """
-        pass
+        '''
+        if self.current_command in ARITHMETIC:
+            return CommandType.C_ARITHMETIC
+        cmd_type = {
+            PUSH : CommandType.C_PUSH,
+            POP : CommandType.C_POP
+            #Can add more command types here
+        }
+        return cmd_type[self.current_command]
 
     def arg1(self):
-        """
-        Returns the first argument of the current command. In the case of C_ARITHMETIC, the command (add, pop, ...) is returned
+        '''
+        Returns the first argument of the current command. In the case of C_ARITHMETIC, the command (add, sub, ...) is returned
         Should not be called if the type is C_RETURN
-        """
-        pass
+        '''
+        return self.current_command.split(' ')[0]
 
     def arg2(self):
-        """
+        '''
         Returns the second argument of the current command. Should only be called for the command types:
             C_PUSH
             C_POP
             C_FUNCTION
             C_CALL
-        """
-        pass
+        '''
+        return int(self.current_command.split(' ')[1])
 
 class CodeWriter:
-    """
+    '''
     Generates the assembly code from parsed VM Commands
-    """
+    '''
     def __init__(self, out_file_name):
-        """
-        Preparaes the output file to write into
-        """
-        pass
+        '''
+        Prepares the output file to write into
+        '''
+        self.file = open(out_file_name, 'w')
 
-    def write_araithmetic(self, command):
-        """
+    def write_arithmetic(self, command):
+        '''
         Writes to the output file the assembly code that implements the given arithmetic command
-        """
+        '''
         pass
 
     def write_push_pop(self, cmd_type, segment, index):
-        """
+        '''
         Writes to the output file the assembly code that implements the given push/pop command
-        """
+        '''
         pass
 
     def close(self):
-        """
+        '''
         Closes the output file
-        """
-        pass
+        '''
+        self.file.close()
 
-def translate(from_file, to_file):
-    pass
+def translate(parser, writer):
+    while parser.has_more_commands():
+        parser.advance()
+        print(parser.current_command)
+        print('^{arg1}'.format(arg1=parser.arg1()))
+
+def translate_files(files, write_name):
+    writer = CodeWriter(write_name)
+    for file_name in files:
+        print('parsing {file}'.format(file=file_name))
+        parser = Parser(file_name)
+        translate(parser, writer)
+
+    writer.close()
+
+def get_write_path(dir, name):
+    write_path = Path(dir)
+    return write_path.joinpath(name + ASSEMBLY_EXTENSION)
 
 def main():
-    #read from file
-    read_name = sys.argv[1]
-    name, _ = os.path.basename(read_name).split('.')
-    write_name = "{dir}{sep}{name}{ext}".format(
-        dir=os.path.dirname(read_name),
-        sep=os.sep,
-        name=name,
-        ext='.asm')
+    path = Path(sys.argv[1])
+    files = []
+    write_name = ''
+    if path.is_file():
+        files.append(path)
+        name, _ = path.name.split('.')
+        write_name = get_write_path(path.parent, name)
+    else:
+        for file_name in path.iterdir():
+            if file_name.suffix == VM_EXTENSION:
+                files.append(file_name)
+        write_name = get_write_path(path.parent, path.name)
 
-    #perform the translation
-    translate(read_name, write_name)
-    print("Successfully translated {file} to {output}".format(
-        file=os.path.basename(read_name),
-        output=write_name))
+    print('translating {files}->{output}'.format(
+        files=files,
+        output=write_name
+    ))
+
+    translate_files(files, write_name)
 
 if __name__ == '__main__':
     main()
